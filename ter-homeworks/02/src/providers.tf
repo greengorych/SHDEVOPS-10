d6dafa663e04aa65e079091fff4937decafd6960
root <root@greengorych.>
2024-11-18 14:58:31 +0100

    Update ter-homeworks 02 Task 1

--- ter-homeworks/02/src/providers.tf
+++ ter-homeworks/02/src/providers.tf
@@ -6,11 +6,10 @@ terraform {
   }
   required_version = ">=1.5"
 }
-
 provider "yandex" {
   # token     = var.token
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.default_zone
-  service_account_key_file = file("~/.authorized_key.json")
+  service_account_key_file = file("~/authorized_key.json")
 }
--- ter-homeworks/02/src/variables.tf
+++ ter-homeworks/02/src/variables.tf
@@ -1,16 +1,13 @@
 ###cloud vars
 
-
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
-
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
-
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
@@ -21,18 +18,16 @@ variable "default_cidr" {
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
-
 variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network & subnet name"
 }
 
-
 ###ssh vars
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILSOXDGT/6lPKrHTx2ZnVrT48T23DMHMy7YZpyEF6rOa root@greengorych"
   description = "ssh-keygen -t ed25519"
 }
