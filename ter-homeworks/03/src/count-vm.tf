f82d84e77e0c14bc249e0d4346cf83d06cb97cc4
root <root@greengorych.>
2024-11-19 15:05:14 +0100

    Update ter-homeworks 03 Task 2

---
+++ ter-homeworks/03/src/count-vm.tf
@@ -0,0 +1,32 @@
+data "yandex_compute_image" "ubuntu" {
+  family          = var.os_family
+}
+
+resource "yandex_compute_instance" "web" {
+  count           = 2
+  name            = "web-${count.index + 1}"
+  platform_id     = var.platform_id
+  zone            = var.default_zone
+
+  resources {
+    cores         = var.vms_resources.web.cores
+    memory        = var.vms_resources.web.memory
+    core_fraction = var.vms_resources.web.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id    = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible   = true
+  }
+  network_interface {
+    subnet_id     = yandex_vpc_subnet.develop.id
+    nat           = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = var.vms_metadata
+
+}
--- ter-homeworks/03/src/variables.tf
+++ ter-homeworks/03/src/variables.tf
@@ -1,19 +1,15 @@
-###cloud vars
 variable "token" {
   type        = string
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
-
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
-
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
-
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
@@ -24,9 +20,40 @@ variable "default_cidr" {
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
-
 variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network&subnet name"
+}
+variable "os_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+}
+variable "platform_id" {
+  type        = string
+  default     = "standard-v3"
+}
+variable "vms_resources" {
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+  }))
+  default = {
+    web = {
+      cores         = 2
+      memory        = 1
+      core_fraction = 20
+    }
+  }
+}
+variable "vms_metadata" {
+  type = object({
+    serial-port-enable = number
+    ssh-keys           = string
+  })
+  default = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILSOXDGT/6lPKrHTx2ZnVrT48T23DMHMy7YZpyEF6rOa root@greengorych"
+  }
 }
